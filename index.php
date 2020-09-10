cdd129ae24aa25ddb0a7893f55643ef22391a4cf
glennpaulmabao <64250849+glennpaulmabao@users.noreply.github.com>
2020-09-11 05:52:30 +0800

    update

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "glennpaulmabao@yahoo.com.ph"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['name'];
-    $phone = $_POST['phone'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $name . " " . $email . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
-
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
