b6145dee5b69b2a56660fc8420b6059897bf17eb
glennpaulmabao <64250849+glennpaulmabao@users.noreply.github.com>
2020-09-11 07:44:29 +0800

    New
    These are the new updates Hope it works

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
+<link rel="stylesheet" href="assets/style.css">
+<title>PCN Strategies | Official Website</title>
+</head>
+<body>
+
+
 <?php 
 
     if(isset($_POST['btn-send']))
@@ -26,21 +41,6 @@
         header("location:index.php");
     }
 ?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<meta name="Description" content="Enter your description here"/>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css">
-<link rel="stylesheet" href="assets/style.css">
-<title>PCN Strategies | Official Website</title>
-</head>
-<body>
 
     <div class="container-fluid topheader">
         <div class="container">
